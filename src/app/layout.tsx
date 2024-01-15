3691488965f07fd0e1f8468425e4db066a186e4b
yang <zy892065502@gmail.com>
2024-01-15 15:42:10 +0800

    add analystic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Prappy',
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <body>{children}</body>
+        <body>{children}
+        <Analytics />
+        </body>
     </html>
   )
 }
